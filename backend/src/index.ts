9d3ac12b1412d7ab73f7108eb6b138e5c2da3205
Swetony <swetonyancelmo@gmail.com>
2025-10-23 15:38:44 -0300

    refactor: Refatora CRUD para usar o Prisma

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,43 +1,35 @@
 import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 const app = express();
 app.use(express.json());
 const PORT = 3001;
 
-interface ITarefa {
-  id: number;
-  descricao: string;
-  concluida: boolean;
-}
-
-let tarefas: ITarefa[] = [];
-
 // GET | url: /tarefas
-app.get('/tarefas', (req: Request, res: Response) => {
-  return res.status(200).json(tarefas);
+app.get('/tarefas', async (req: Request, res: Response) => {
+  const todasTarefa = await prisma.tarefa.findMany();
+  return res.status(200).json(todasTarefa);
 });
 
 // POST | url: /tarefas
-app.post('/tarefas', (req: Request, res: Response) => {
+app.post('/tarefas', async (req: Request, res: Response) => {
   const { descricao } = req.body;
 
   if(!descricao) {
     return res.status(400).json({ message: "A descrição é obrigatória." })
   }
 
-  const novaTarefa: ITarefa = {
-    id: tarefas.length + 1,
-    descricao: descricao,
-    concluida: false
-  };
-
-  tarefas.push(novaTarefa);
-
-  return res.status(201).json(novaTarefa); // 201 CREATED
+  const novaTarefa = await prisma.tarefa.create({
+    data: {
+      descricao: descricao
+    },
+  });
+  return res.status(201).json(novaTarefa);
 });
 
 // UPDATE | url: /tarefas/id
-app.put('/tarefas/:id', (req: Request, res: Response) => {
+app.put('/tarefas/:id', async (req: Request, res: Response) => {
   const id = parseInt(req.params.id, 10);
 
   if (Number.isNaN(id)) {
@@ -46,42 +38,36 @@ app.put('/tarefas/:id', (req: Request, res: Response) => {
 
   const { descricao, concluida } = req.body;
 
-  const tarefaIndex = tarefas.findIndex(tarefa => tarefa.id === id);
-
-  if (tarefaIndex === -1) {
-    return res.status(404).json({ message: 'Tarefa não encontrada.' });
+  try {
+    const tarefaAtualizada = await prisma.tarefa.update({
+      where: { id: id },
+      data: {
+        descricao: descricao,
+        concluida: concluida,
+      },
+    });
+    return res.status(200).json(tarefaAtualizada);
+  } catch(error){
+    return res.status(404).json({ message: "Tarefa não encontrada." });
   }
-
-  const tarefaExistente = tarefas[tarefaIndex];
-
-  const tarefaAtualizada: ITarefa = {
-    id: tarefaExistente.id,
-    descricao: descricao !== undefined ? descricao : tarefaExistente.descricao,
-    concluida: concluida !== undefined ? concluida : tarefaExistente.concluida,
-  };
-
-  tarefas[tarefaIndex] = tarefaAtualizada;
-
-  return res.status(200).json(tarefaAtualizada);
 });
 
 // DELETE | url: /tarefas/id
-app.delete('/tarefas/:id', (req: Request, res: Response) => {
+app.delete('/tarefas/:id', async (req: Request, res: Response) => {
   const id = parseInt(req.params.id, 10);
 
   if (Number.isNaN(id)) {
     return res.status(400).json({ message: 'ID inválido.' });
   }
 
-  const tarefaIndex = tarefas.findIndex(tarefa => tarefa.id === id);
-
-  if (tarefaIndex === -1) {
-    return res.status(404).json({ message: 'Tarefa não encontrada.' });
+  try {
+    await prisma.tarefa.delete({
+      where: { id: id },
+    });
+    return res.sendStatus(204);
+  } catch (error){
+    return res.status(404).json({ message: "Tarefa não encontrada." });
   }
-  //               Index    Quantidade
-  tarefas.splice(tarefaIndex, 1);
-
-  return res.sendStatus(204);
 });
 
 
